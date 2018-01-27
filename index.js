ffe9ecaca8b2aae74090914368462474c7379b2a
Oluwakamiye Adelemoni <oluwakamiye.adelemoni@gmail.com>
2018-01-27 11:51:02 +0300

    Make page token environment variable

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.post('/panda/webhook', function (req, res) {
 function sendMessage(recipientId, message) {  
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token: PAGE_TOKEN},
+        qs: {access_token: process.env.PAGE_TOKEN},
         method: 'POST',
         json: {
             recipient: {id: recipientId},
